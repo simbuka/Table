334b57b4bd7962fad500d11c1eca0b11c963747c
Lukas <theluksys3@gmail.com>
2019-04-05 11:45:11 +0300

    fix(table-pagination): arrows to text

--- src/components/table-pagination/table-pagination.tsx
+++ src/components/table-pagination/table-pagination.tsx
@@ -23,24 +23,6 @@ export class TablePagination {
 	 */
 	@Event() onPageChange: EventEmitter<IOnPageChange>;
 
-	private arrowIcon(type: 'previous' | 'next') {
-		return (
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				class="block h-5 align-middle"
-				style={{
-					transform: type === 'previous' ? 'rotate(180deg)' : ''
-				}}
-			>
-				<path
-					class="fill-current"
-					d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"
-				/>
-			</svg>
-		);
-	}
-
 	private createButton({
 		type,
 		active
@@ -51,7 +33,7 @@ export class TablePagination {
 		return (
 			<div
 				class={`
-					bg-white p-1 m-3 border-grey-light border rounded-full uppercase font-bold
+					p-1 m-3 uppercase font-bold
                     ${active ? 'cursor-pointer hover:bg-grey-light' : 'opacity-50'}
                 `}
 				onClick={() => {
@@ -60,7 +42,7 @@ export class TablePagination {
 					}
 				}}
 			>
-				{this.arrowIcon(type)}
+				{type}
 			</div>
 		);
 	}
